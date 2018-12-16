e3cb5dcb18782a9f96f4dd362b41f23adb944876
Abraham Bueno <abueno@localhost.localdomain>
2018-12-15 23:29:38 -0600

    Added a little more styling and a direction to state.

--- marsrover/src/components/Grid.js
+++ marsrover/src/components/Grid.js
@@ -7,29 +7,44 @@ class Grid extends Component {
       position: [0, 0],
       left: 0,
       right: 0,
-      direction: "x"
+      direction: "N"
     };
   }
   handleChange = e => {
     e.preventDefault();
-    this.setState({ [e.targe.name]: e.target.value });
+    this.setState({ [e.targe.name]: 1 });
+  };
+  handleSubmit = () => {
+    this.setState({});
   };
   render() {
     return (
       <div>
-        <div className="rover" />
-        <div>Current position is: {this.state.position} </div>
+        <div className="rover">
+          <div className="head">Head</div>
+        </div>
+        <div>
+          Current position is: {this.state.position} {this.state.direction}
+        </div>
         <button
           className="direction"
-          name="direction"
+          name="Ldirection"
           onChange={this.handleChange}
-          value={this.state.position}
+          value={this.state.left}
         >
           Left
         </button>
-        <button className="move">Move Up</button>
-        <button className="move">Move Down What</button>
-        <button className="direction">Right</button>
+        <button className="move">Move</button>
+        {/* <button className="move">Move Down What</button> */}
+        <button
+          className="direction"
+          name="Ldirection"
+          onChange={this.handleChange}
+          value={this.state.right}
+        >
+          Right
+        </button>
+        <div>Position moved to: {this.state.direction}</div>
       </div>
     );
   }
