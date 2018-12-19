cdfa74d26e80ae4c32118713668f8346c6e07760
Abraham Bueno <abueno@localhost.localdomain>
2018-12-18 21:10:09 -0600

    Removed console log of left and right state, as it was removed. Added more comments to explain the state of the Grid component.

--- marsrover/src/components/Grid.js
+++ marsrover/src/components/Grid.js
@@ -12,12 +12,28 @@ class Grid extends Component {
       xGrids: 0,
       yGrids: 0,
 
+      // Clockwise angle
       direction: 90,
+
+      // Counter-clockwise angle
+      // needed for CSS transform
       angle: 90,
+
+      // Direction as NSEW
       dir: "N",
+
+      // When the rover was going out of boundary.
+      //  Will stop before going out of boundary
       danger: false,
+
+      // input for the commands
       inputCommand: "",
+
+      // the queue of commands from input
+      // converted to an array to keep track
       commandQueu: [],
+
+      // the grid dimensions from input
       inputGrid: ""
     };
   }
@@ -200,11 +216,6 @@ class Grid extends Component {
   };
 
   render() {
-    console.log(
-      "What is the left and right values for",
-      this.state.left,
-      this.state.right
-    );
     return (
       <div>
         <GridDisplay
