d91e7519d31fb64ff648e2e9c169ec3e65593d67
Abraham Bueno <abueno@localhost.localdomain>
2018-12-20 21:29:28 -0600

    Adding redux to manage state. This app has a lot of state to keep track of, because there's a lot of state that changes for each rover. Each rover has it's own state, that must not be mutated by the changes of each other, so to prevent mutations, it's best to use redux.

--- marsrover/src/index.js
+++ marsrover/src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// redux
+import reducers from "./reducers";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+// dev tools
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(reducers, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ marsrover/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+  position1: [],
+  position2: []
+};
+
+const roverStates = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default roverStates;
