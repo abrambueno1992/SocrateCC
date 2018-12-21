00c2980ac87bb160ac3d7a42bc42774ea4d871cf
Abraham Bueno <abueno@localhost.localdomain>
2018-12-20 23:58:19 -0600

    Moved some input and functionality into controller. Will need to add more.

--- marsrover/src/components/Controller.js
+++ marsrover/src/components/Controller.js
@@ -23,6 +23,27 @@ class Controller extends Component {
   render() {
     return (
       <div>
+        <div>
+          <h3>Current positions of Rovers</h3>
+          <span>
+            Rover 1: (x: {this.state.position[1]}, y:
+            {this.state.position[0]}) {this.state.dir}
+          </span>
+          <span>
+            Rover 2: (x: {this.state.position2[1]}, y:
+            {this.state.position2[0]}) {this.state.dir2}
+          </span>
+        </div>
+        <div>
+          <h3>Enter the max x and y coordinates, space separated: x y</h3>
+          <input
+            type="text"
+            name="inputGrid"
+            placeHolder="x y"
+            value={this.state.inputGrid}
+            onChange={this.handleGrid}
+          />
+        </div>
         <Rover1 />
         <Rover2 />
       </div>
