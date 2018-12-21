e62beafac3fe665b300b9b58b6d252ce58bcc90a
Abraham Bueno <abueno@localhost.localdomain>
2018-12-20 23:09:12 -0600

    More initial setup

---
+++ marsrover/src/components/Rover1.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+// redux
+import { connect } from "react-redux";
+import { setPosR1 } from "../actions/rover1";
+class Rover2 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  render() {
+    return <div />;
+  }
+}
---
+++ marsrover/src/components/Rover2.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// redux
+import { connect } from "react-redux";
+import { setPosR2 } from "../actions/rover2";
+class Rover2 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+  render() {
+    return <div />;
+  }
+}
