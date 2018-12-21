1affccdacc96f489d000912c259cf07ed1129e4a
Abraham Bueno <abueno@localhost.localdomain>
2018-12-21 11:26:44 -0600

    Added redux state to the controller component.

--- marsrover/src/components/Controller.js
+++ marsrover/src/components/Controller.js
@@ -57,7 +57,35 @@ class Controller extends Component {
   }
 }
 
-const mapStateToProps = state => {};
+const mapStateToProps = state => {
+  return {
+    // Position of the rovers
+    position: state.position,
+    position2: state.position2,
+    // Grid
+    xGrids: state.xGrids,
+    yGrids: state.yGrids,
+
+    // Clockwise angle
+    direction: state.direction,
+    direction2: state.direction2,
+    // Counter-clockwise angle
+    // needed for CSS transform
+    angle: state.angle,
+    angle2: state.angle2,
+    // Direction as NSEW
+    dir: state.dir,
+    dir2: state.dir2,
+    // When the rover was going out of boundary.
+    //  Will stop before going out of boundary
+    danger: state.danger,
+
+    // the queue of commands from input
+    // converted to an array to keep track
+    commandQueu: state.commandQueu,
+    commandQueu2: state.commandQueu2
+  };
+};
 
 export default connect(
   mapStateToProps,
