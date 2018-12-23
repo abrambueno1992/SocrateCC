5b193dc4651192244d0f93c25bbbf16fc870c2a1
Abraham Bueno <abueno@localhost.localdomain>
2018-12-22 23:37:11 -0600

    First tests for setGrid has been written.

--- marsrover/src/actions/__tests__/controller.tests.js
+++ marsrover/src/actions/__tests__/controller.tests.js
@@ -4,3 +4,17 @@ import {
   EXECUTE_COMMANDS,
   executeCommands
 } from "../controller";
+
+describe("setGrid", () => {
+  it("has the correct type", () => {
+    const action = setGrid();
+    expect(action.type).toEqual(SET_GRID);
+  });
+
+  it("has the correct payload", () => {
+    const action = setGrid(3, 5);
+    expect(action.x).toEqual(3);
+    expect(action.y).toEqual(5);
+    expect(action.grid).toEqual(true);
+  });
+});
