072794764fac4bfccf06ab0039cd94d086caa6f2
Abraham Bueno <abueno@localhost.localdomain>
2018-12-24 16:49:52 -0600

    Deleted unnecessary code from Root.js that was used to debug.

--- marsrover/src/Root.js
+++ marsrover/src/Root.js
@@ -4,15 +4,11 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers";
 
-// const store = createStore(
-//   reducers,
-//   composeWithDevTools()
-//   // applyMiddleware(...middleware)
-//   // other store enhancers if any
-// );
-// import { composeWithDevTools } from "redux-devtools-extension";
-
 const store = createStore(reducers, composeWithDevTools());
 export default ({ children }) => {
-  return <Provider store={createStore(reducers, composeWithDevTools())}>{children}</Provider>;
+  return (
+    <Provider store={createStore(reducers, composeWithDevTools())}>
+      {children}
+    </Provider>
+  );
 };
