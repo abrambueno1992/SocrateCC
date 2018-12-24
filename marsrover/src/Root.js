1efeb6d2d8607290a9cd01d9f222c981394af4b4
Abraham Bueno <abueno@localhost.localdomain>
2018-12-24 16:52:54 -0600

    Deleted code that was there, which was deleted. Added the proper logic to the Root.js

--- marsrover/src/Root.js
+++ marsrover/src/Root.js
@@ -2,9 +2,8 @@ import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import roverStates from "./reducers";
+import reducers from "./reducers";
 
-const store = createStore(reducers, composeWithDevTools());
 export default ({ children }) => {
   return (
     <Provider store={createStore(reducers, composeWithDevTools())}>
