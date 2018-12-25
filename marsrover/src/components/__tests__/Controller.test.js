cede4df256bef206a95a9906d2c5158dde0c4418
Abraham Bueno <abueno@localhost.localdomain>
2018-12-25 14:52:58 -0600

    completed beforeEach, afterEach, and created one it ocndition. Will add conditions for Components to be inside of the controller.

--- marsrover/src/components/__tests__/Controller.test.js
+++ marsrover/src/components/__tests__/Controller.test.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { mount } from "enzyme";
-import Controller from "../Controller";
+// Root needed for wrapper
 import Root from "../../Root";
+// Components
+import Controller from "../Controller";
+import GridDisplay from "../GridDisplay";
+import Rover1 "../Rover1";
+import Rover2 "../Rover2";
 
 let wrapped;
 beforeEach(() => {
@@ -11,3 +16,16 @@ beforeEach(() => {
         </Root>
     )
 });
+
+afterEach(() => {
+    wrapped.unmount();
+})
+
+it('should have a GridDisplay component', () => {
+    expect(wrapped.find(GridDisplay).length).toEqual(1);
+})
+
+it('should ', () => {
+  
+})
+
