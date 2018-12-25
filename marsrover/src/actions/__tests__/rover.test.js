8e8d09764123586e666fd4e28002445499e8a66d
Abraham Bueno <abueno@localhost.localdomain>
2018-12-24 21:25:05 -0600

    Initial import of constants and functions.

---
+++ marsrover/src/actions/__tests__/rover.test.js
@@ -0,0 +1,11 @@
+import {
+  SET_POSITION_R1,
+  EXECUTE_CMD_DIR1,
+  EXECUTE_CMD_MV1,
+  SET_POSITION_R2,
+  EXECUTE_CMD_DIR2,
+  EXECUTE_CMD_MV2,
+  setPosR,
+  executeCMDdir,
+  executeCMDmv
+} from "../rover";
