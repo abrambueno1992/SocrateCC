127d3ee9638bf997605ae1fc1ac172be913c328a
Abraham Bueno <abueno@localhost.localdomain>
2018-12-26 23:35:52 -0600

    Rover2.test.js was not previously saved. I'm now pushing the code. It has the same tests as Rover1.test.js, except Rover2 component is mounted for the testing.

---
+++ marsrover/src/components/__tests__/Rover2.test.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { mount } from "enzyme";
+import Root from "../../Root";
+// Components
+import Rover2 from "../Rover2";
+
+let wrapped;
+beforeEach(() => {
+  wrapped = mount(
+    <Root>
+      <Rover2 />
+    </Root>
+  );
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("should have input for inputCoordinates and  inputCommand", () => {
+  expect(wrapped.find(".inputCoordinates").length).toEqual(1);
+  expect(wrapped.find(".inputCommand").length).toEqual(1);
+});
+
+describe("the text input", () => {
+  beforeEach(() => {
+    wrapped.find(".inputCoordinates").simulate("change", {
+      target: {
+        value: "6 7",
+        name: "inputCoordinates"
+      }
+    });
+    wrapped.update();
+  });
+  it("should have an input that users can type in", () => {
+    expect(wrapped.find(".inputCoordinates").prop("value")).toEqual("6 7");
+  });
+});
+
+describe("the text input for commands", () => {
+  beforeEach(() => {
+    wrapped.find(".inputCoordinates").simulate("change", {
+      target: { value: "LRRRMMMLRLRM", name: "inputCoordinates" }
+    });
+    wrapped.update();
+  });
+  it("should have an input that users can type in", () => {
+    expect(wrapped.find(".inputCoordinates").prop("value")).toEqual(
+      "LRRRMMMLRLRM"
+    );
+  });
+});
