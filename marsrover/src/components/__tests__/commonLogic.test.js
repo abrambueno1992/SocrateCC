c837d1b00e69194d6da81bdc12e589f1e012287e
Abraham Bueno <abueno@localhost.localdomain>
2018-12-27 22:01:21 -0600

    Added test for directionLogic inside of commonLogic.

--- marsrover/src/components/__tests__/commonLogic.test.js
+++ marsrover/src/components/__tests__/commonLogic.test.js
@@ -1,7 +1,7 @@
 const funcs = require("../commonLogic");
 
 describe("should return updated coordinates", () => {
-  it("should return the proper logic", () => {
+  it("should return the proper logical coordinates", () => {
     const pDir = "W";
     const xCoor = 3;
     const yCoor = 5;
@@ -12,6 +12,23 @@ describe("should return updated coordinates", () => {
     state.position = [xCoor, yCoor];
     state.coordinates = "";
     const action = funcs.coordinateLogic(pDir, xCoor, yCoor);
+    expect(action).toEqual(state);
+  });
+});
+
+describe("should return updated direction", () => {
+  it("should return the proper logical direction", () => {
+    const currentDir = 90;
+    const split = ["M", "M", "R"];
+    const end = 1;
+    const state = {
+      direction: currentDir,
+      dir: "N",
+      angle: 90,
+      commandQueu: split,
+      execute: end
+    };
+    const action = funcs.directionLogic(currentDir, split, 1);
     expect(action).toEqual(state);
   });
 });
