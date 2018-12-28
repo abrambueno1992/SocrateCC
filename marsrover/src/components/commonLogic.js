479397802b8f1fe75740714065488c956b64cf90
Abraham Bueno <abueno@localhost.localdomain>
2018-12-27 19:31:39 -0600

    Testing methods to reduce the amount of code inside of the Rover components.

---
+++ marsrover/src/components/commonLogic.js
@@ -0,0 +1,36 @@
+export const coordinateLogic = pDir => {
+  const state = {};
+  if (pDir === "N") {
+    state.direction = 90;
+    state.dir = "N";
+    state.angle = 90;
+    state.position = [xCoor, yCoor];
+    state.coordinates = "";
+    // this.props.setPosR(state, 1);
+    return state;
+  } else if (pDir === "W") {
+    state.direction = 180;
+    state.dir = "W";
+    state.angle = 0;
+    state.position = [xCoor, yCoor];
+    state.coordinates = "";
+    // this.props.setPosR(state, 1);
+    return state;
+  } else if (pDir === "S") {
+    state.direction = 270;
+    state.dir = "S";
+    state.angle = 270;
+    state.position = [xCoor, yCoor];
+    state.coordinates = "";
+    // this.props.setPosR(state, 1);
+    return state;
+  } else {
+    state.direction = 0;
+    state.dir = "E";
+    state.angle = 180;
+    state.position = [xCoor, yCoor];
+    state.coordinates = "";
+    // this.props.setPosR(state, 1);
+    return state;
+  }
+};
