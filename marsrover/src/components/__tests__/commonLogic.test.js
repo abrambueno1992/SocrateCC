0a05f95fc918a2d25cce6b48197307b447280796
Abraham Bueno <abueno@localhost.localdomain>
2018-12-27 22:16:36 -0600

    Added more case tests for coordinateLogic.

--- marsrover/src/components/__tests__/commonLogic.test.js
+++ marsrover/src/components/__tests__/commonLogic.test.js
@@ -2,17 +2,42 @@ const funcs = require("../commonLogic");
 
 describe("should return updated coordinates", () => {
   it("should return the proper logical coordinates", () => {
-    const pDir = "W";
-    const xCoor = 3;
-    const yCoor = 5;
+    let pDir = "W";
+    let xCoor = 3;
+    let yCoor = 5;
     const state = {};
     state.direction = 180;
     state.dir = "W";
     state.angle = 0;
     state.position = [xCoor, yCoor];
     state.coordinates = "";
+    // pDir === "W"
     const action = funcs.coordinateLogic(pDir, xCoor, yCoor);
     expect(action).toEqual(state);
+
+    // pDir === "N"
+    pDir = "N";
+    state.dir = "N";
+    state.angle = 90;
+    state.direction = 90;
+    const action2 = funcs.coordinateLogic(pDir, xCoor, yCoor);
+    expect(action2).toEqual(state);
+
+    // pDir === "S"
+    pDir = "S";
+    state.dir = "S";
+    state.angle = 270;
+    state.direction = 270;
+    const action3 = funcs.coordinateLogic(pDir, xCoor, yCoor);
+    expect(action3).toEqual(state);
+
+    // pDir === "E"
+    pDir = "E";
+    state.dir = "E";
+    state.angle = 180;
+    state.direction = 0;
+    const action4 = funcs.coordinateLogic(pDir, xCoor, yCoor);
+    expect(action4).toEqual(state);
   });
 });
 
