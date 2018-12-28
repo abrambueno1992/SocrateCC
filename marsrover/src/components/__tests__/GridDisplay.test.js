4217f87af667cd8c41275fcd552dc661b784d385
Abraham Bueno <abueno@localhost.localdomain>
2018-12-27 18:52:34 -0600

    Added Rover1, Rover2 to the mount. Will test, but will remove based on some initial setup that failed.

--- marsrover/src/components/__tests__/GridDisplay.test.js
+++ marsrover/src/components/__tests__/GridDisplay.test.js
@@ -4,6 +4,8 @@ import Root from "../../Root";
 // Components
 import Controller from "../Controller";
 import GridDisplay from "../GridDisplay";
+import Rover1 from "../Rover1";
+import Rover2 from "../Rover2";
 
 let wrapped;
 beforeEach(() => {
@@ -11,6 +13,8 @@ beforeEach(() => {
     <Root>
       <Controller>
         <GridDisplay />
+        <Rover1 />
+        <Rover2 />
       </Controller>
     </Root>
   );
@@ -23,6 +27,24 @@ afterEach(() => {
 it("should have input for inputCoordinates and  inputCommand", () => {
   expect(wrapped.find(".what").length).toEqual(1);
   //   expect(wrapped.find(".inputCommand").length).toEqual(1);
+});
+
+describe("the text input", () => {
+  beforeEach(() => {
+    wrapped.find("input.inputGrid").simulate("change", {
+      target: { value: "6 7" }
+    });
+    wrapped.update();
+  });
+  it("should have an input that users can type in", () => {
+    expect(wrapped.find(".inputGrid").prop("value")).toEqual("6 7");
+  });
+
+  it("should not clear inputGrid value", () => {
+    wrapped.find("button.inputGridBtn").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("input.inputGrid").prop("value")).toEqual("6 7");
+  });
 });
 
 // describe("the text input", () => {
